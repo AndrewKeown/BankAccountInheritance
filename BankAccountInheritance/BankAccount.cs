52b6dc003b903665d1b5ff030054c09dbf25a7fb
AndrewKeown <s00238928@atu.ie>
2023-05-05 09:46:39 +0100

    Bank Account

--- BankAccountInheritance/BankAccount.cs
+++ BankAccountInheritance/BankAccount.cs
@@ -8,11 +8,24 @@ namespace BankAccountInheritance
 {
     internal class BankAccount
     {
+        string _accountNumber;
+
+
+        public string AccountNumber
+        {
+            get { return _accountNumber; }
+            set { _accountNumber = value; }
+        }
+
         public double Balance { get; private set; }
 
         public BankAccount() { }
 
-        public BankAccount(double balance) { this.Balance = balance; }
+        public BankAccount(string accountNumber,double balance)
+        {
+            this.Balance = balance;
+            this._accountNumber = accountNumber;
+        }
 
         public override string ToString()
         {
