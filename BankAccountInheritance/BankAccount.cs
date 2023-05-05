e8f9b3195852968b80657f956c4680ca035270ee
AndrewKeown <s00238928@atu.ie>
2023-05-05 09:37:06 +0100

    Add project files.

---
+++ BankAccountInheritance/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountInheritance
+{
+    internal class BankAccount
+    {
+        public double Balance { get; private set; }
+
+        public BankAccount() { }
+
+        public BankAccount(double balance) { this.Balance = balance; }
+
+        public override string ToString()
+        {
+            return $"Balance: {Balance}";
+        }
+    }
+}
---
+++ BankAccountInheritance/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankAccountInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
